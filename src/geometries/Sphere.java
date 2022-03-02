eccf8f65f17dd739f9cdd66425ca312c8902e58f
TamariIv <taivgi@g.jct.ac.il>
2022-03-02 21:53:24 +0200

    fix sphere

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -7,6 +7,26 @@ public class Sphere implements Geometry {
     Point p0;
     double radius;
 
+    public Sphere(Point p0, double radius) {
+        this.p0 = p0;
+        this.radius = radius;
+    }
+
+    public Point getP0() {
+        return p0;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Sphere{" +
+                "p0=" + p0 +
+                ", radius=" + radius +
+                '}';
+    }
 
     @Override
     public Vector getNormal(Point p) {
