daee0dc5cb2af19c7963547dbb2b066c1360f85b
TamariIv <taivgi@g.jct.ac.il>
2022-03-03 15:32:16 +0200

    add documentation

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -7,14 +7,25 @@ public class Cylinder implements Geometry {
 
     double height;
 
+    /**
+     * cylinder constructor
+     * @param height parameter for height
+     */
     public Cylinder(double height) {
         this.height = height;
     }
 
+    /** getter for height
+     * @return the height
+     */
     public double getHeight() {
         return height;
     }
 
+    /**
+     * override toString
+     * @return string representing the cylinder
+     */
     @Override
     public String toString() {
         return "Cylinder{" +
@@ -22,6 +33,11 @@ public class Cylinder implements Geometry {
                 '}';
     }
 
+    /**
+     * implement interface Geometry function
+     * @param p the point from which we want the normal
+     * @return the perpendicular vector to the point that was received
+     */
     @Override
     public Vector getNormal(Point p) {
         return null;
