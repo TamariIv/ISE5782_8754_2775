a34c23b9ae184f4f211c53657e85905bd9cce4e9
hodayaAsh <hoashken@g.jct.ac.il>
2022-03-12 22:14:37 +0200

    adding tests

--- unitTest/primitives/PointTest.java
+++ unitTest/primitives/PointTest.java
@@ -2,16 +2,21 @@ package primitives;
 
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Math.sqrt;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
 
+    Point p1 = new Point(1, 2, 3);
+    Point p2 = new Point(-2, -4, -6);
     @Test
     void testDistanceSquared() {
+        assertEquals(p1.distanceSquared(p2),126.0,"ERROR: DistanceSquared() wrong value");
     }
 
     @Test
     void testDistance() {
+        assertEquals(p1.distance(p2),sqrt(126),"ERROR: Distance() wrong value");
     }
 
     @Test
@@ -19,10 +24,12 @@ class PointTest {
      * Test method for {@link Point#add(Vector)}.
      */
     void testAdd() {
-            fail("Not yet implemented");
+        assertEquals(p1.add(new Vector(-1, -2, -3)),new Point(0, 0, 0),"ERROR: Point + Vector does not work correctly");
+
         }
 
     @Test
     void testSubtract() {
+        assertEquals(new Point(2, 3, 4).subtract(p1),new Vector(1, 1, 1),"ERROR: Point - Point does not work correctly");
     }
 }
--- unitTest/primitives/VectorTest.java
+++ unitTest/primitives/VectorTest.java
@@ -2,7 +2,9 @@ package primitives;
 
 import org.junit.jupiter.api.Test;
 
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
+import static primitives.Util.isZero;
 
 class VectorTest {
 
@@ -24,6 +26,9 @@ class VectorTest {
     @Test
     void testLength() {
 
+        // ============ Equivalence Partitions Tests ==============
+        assertTrue(!isZero(v1.lengthSquared() - 14),"lengthSquared() wrong value");
+        assertTrue(!isZero(new Vector(0, 3, 4).length() - 5),"lengthSquared() wrong value");
     }
 
     @Test
@@ -36,21 +41,65 @@ class VectorTest {
 
     @Test
     void testDotProduct() {
+
+        // =============== Boundary Values Tests ==================
+        assertTrue(!isZero(v1.dotProduct(v3)), "dotProduct() for orthogonal vectors is not zero");
+
+        // ============ Equivalence Partitions Tests ==============
+        assertTrue(!isZero(v1.dotProduct(v2) + 28), "dotProduct() for orthogonal vectors is not zero");
+
     }
 
+    /**
+     * Test method for {@link primitives.Vector#crossProduct(primitives.Vector)}.
+     */
     @Test
     void testCrossProduct() {
-    }
+
+        Vector v1 = new Vector(1, 2, 3);
+
+            // ============ Equivalence Partitions Tests ==============
+            Vector v2 = new Vector(0, 3, -2);
+            Vector vr = v1.crossProduct(v2);
+
+            // TC01: Test that length of cross-product is proper (orthogonal vectors taken
+            // for simplicity)
+            assertEquals(v1.length() * v2.length(), vr.length(), 0.00001, "crossProduct() wrong result length");
+
+            // TC02: Test cross-product result orthogonality to its operands
+            assertTrue(isZero(vr.dotProduct(v1)), "crossProduct() result is not orthogonal to 1st operand");
+            assertTrue(isZero(vr.dotProduct(v2)), "crossProduct() result is not orthogonal to 2nd operand");
+
+            // =============== Boundary Values Tests ==================
+            // TC11: test zero vector from cross-productof co-lined vectors
+            Vector v3 = new Vector(-2, -4, -6);
+            assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v3),
+                    "crossProduct() for parallel vectors does not throw an exception");
+        }
 
     @Test
     void testNormalize() {
+//        Vector v = new Vector(1, 2, 3);
+//        Vector u = v.normalize();
+//        assertTrue (!isZero(u.length() - 1),"the normalized vector is not a unit vector");
+//
+//        v.crossProduct(u);
+//        assertThrows(IllegalArgumentException.class, ()-> v1.crossProduct())
+//            out.println("ERROR: the normalized vector is not parallel to the original one");
+//
+//
+//        if (v.dotProduct(u) < 0)
+//            out.println("ERROR: the normalized vector is opposite to the original one");
+
     }
 
     @Test
     void testAdd() {
+
     }
 
     @Test
     void testScale() {
+
     }
 }
