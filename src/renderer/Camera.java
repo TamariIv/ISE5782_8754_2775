726652f55c930f54ddac684ace8ddf0547c80e4f
TamariIv <taivgi@g.jct.ac.il>
2022-03-27 14:31:00 +0300

    more camera stuff

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -7,10 +7,14 @@ import primitives.*;
  */
 public class Camera {
 
-    private Vector _vTo;      // vector pointing towards the scene
-    private Vector _vUp;      // vector pointing upwards
+    private Vector _vTo;            // vector pointing towards the scene
+    private Vector _vUp;            // vector pointing upwards
     private Vector _vRight;
     private  Point _p0;             // camera eye
+    
+    private double _distance;       // camera distance from view plane
+    private double _width;          // view plane width
+    private double _height;         // view plane height
 
     /**
      *
@@ -23,7 +27,7 @@ public class Camera {
             throw new IllegalArgumentException("vTo and vUp should be orthogonal");
         _p0 = p0;
 
-        camera_vTo = vTo.normalize();
+        _vTo = vTo.normalize();
         _vUp = vUp.normalize();
 
         _vRight = _vTo.crossProduct(vUp);
@@ -37,7 +41,22 @@ public class Camera {
      * @return instance of camera for chaining
      */
     public Camera setVPDistance(double distance){
-        this.
+        _distance = distance
+        return this;
+    }
 
+    /**
+     * set view plane size
+     * @param width physical width
+     * @param height physical height
+     * @return
+     */
+    public Camera setVPSize(double width, double height){
+        _width = width;
+        _height = height;
+        return this;
+    }
+
+    public Ray constructRay(int i, int i1, int i2, int i3) {
     }
 }
