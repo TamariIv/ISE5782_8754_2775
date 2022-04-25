ab3f4ecc7fc0f5495d0ec302bf6f5d4bef35a97f
hodayaAsh <hoashken@g.jct.ac.il>
2022-04-25 21:39:26 +0300

    start exercise 6

--- unitTest/renderer/RenderTests.java
+++ unitTest/renderer/RenderTests.java
@@ -38,7 +38,7 @@ public class RenderTests {
 				.setVPDistance(100) //
 				.setVPSize(500, 500) //
 				.setImageWriter(new ImageWriter("base render test", 1000, 1000))
-				.setRayTracer(new RayTracerBasic(scene));
+				.setRayTracerBase(new RayTracerBasic(scene));
 
 		camera.renderImage();
 		camera.printGrid(100, new Color(YELLOW));
@@ -71,7 +71,7 @@ public class RenderTests {
 				.setVPDistance(100) //
 				.setVPSize(500, 500) //
 				.setImageWriter(new ImageWriter("color render test", 1000, 1000))
-				.setRayTracer(new RayTracerBasic(scene));
+				.setRayTracerBase(new RayTracerBasic(scene));
 
 		camera.renderImage();
 		camera.printGrid(100, new Color(WHITE));
@@ -90,7 +90,7 @@ public class RenderTests {
 		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
 				.setVPDistance(100) //
 				.setVPSize(500, 500).setImageWriter(new ImageWriter("xml render test", 1000, 1000))
-				.setRayTracer(new RayTracerBasic(scene));
+				.setRayTracerBase(new RayTracerBasic(scene));
 		camera.renderImage();
 		camera.printGrid(100, new Color(YELLOW));
 		camera.writeToImage();
