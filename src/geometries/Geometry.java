0e4055d1d4dbbcdf95eedb33bd6d358f107383df
hodayaAsh <hoashken@g.jct.ac.il>
2022-04-25 21:42:10 +0300

    start exercise 6

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -4,7 +4,7 @@ import primitives.Color;
 import primitives.Point;
 import primitives.Vector;
 
-public abstract class Geometry implements Intersectable {
+public abstract class Geometry extends Intersectable {
 
     protected Color emission = Color.BLACK;
 
@@ -15,7 +15,6 @@ public abstract class Geometry implements Intersectable {
     public Color getEmission() {
         return emission;
     }
-
     /**
      * return the normal vector from the shape
      * @param p point to get the normal from {@link Point}
