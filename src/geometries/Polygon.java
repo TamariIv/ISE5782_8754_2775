046b0e4ef3643306e070bac9f2fe6cc36e55aad7
TamariIv <taivgi@g.jct.ac.il>
2022-05-25 21:30:14 +0300

    build image

--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -174,6 +174,23 @@ public class Polygon extends Geometry {
             }
         }
         return List.of(new GeoPoint(this, result.get(0).point));
+
+//        Vector v1  = vertices.get(1).subtract(p0);
+//        Vector v2 = vertices.get(0).subtract(p0);
+//        double sign = v.dotProduct(v1.crossProduct(v2));
+//        if (isZero(sign))
+//            return null;
+//        boolean positive = sign > 0;
+//        for (int i=vertices.size()-1; i>0; i--){
+//            v1=v2;
+//            v2=vertices.get(i).subtract(p0);
+//            sign = alignZero(v.dotProduct(v1.crossProduct(v2)));
+//            if (isZero(sign))
+//                return null;
+//            if (positive != sign>0)
+//                return null;
+//        }
+//        return List.of(new GeoPoint(this, result.get(0).point));
     }
 
 //	@Override
@@ -217,5 +234,6 @@ public class Polygon extends Geometry {
 //		}
 //		return List.of(new GeoPoint(this, result.get(0).point));
 //	}
+
 }
 
