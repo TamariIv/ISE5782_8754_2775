2d726f7f2ac2c82762f994856295dfc574c02c98
TamariIv <taivgi@g.jct.ac.il>
2022-06-12 13:20:42 +0300

    add desk

--- unitTest/miniProject/miniPoject.java
+++ unitTest/miniProject/miniPoject.java
@@ -14,6 +14,155 @@ import scene.Scene;
 
 public class miniPoject {
     private Scene scene = new Scene("Test scene");
+
+
+    @Test
+    public void finalPic4() {
+        Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0))
+                .setVPSize(200, 200).setVPDistance(1000);
+        scene.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
+
+        scene.geometries.add(
+                // ROOM
+                new Polygon( // floor
+                        new Point(-100, -100, 200),
+                        new Point(100, -100, 200),
+                        new Point(60, -60, 100),
+                        new Point(-60, -60, 100))
+                        .setEmission(new Color(255, 230, 205))
+                        .setMaterial(new Material()
+                                .setKd(0.3).setKs(0.8).setShininess(80).setkT(new Double3(0.5))),
+                new Polygon( // back wall
+                        new Point(-60, 60, 100),
+                        new Point(60, 60, 100),
+                        new Point(60, -60, 100),
+                        new Point(-60, -60, 100))
+                        .setEmission(new Color(170,250,255))
+                        .setMaterial(new Material()
+                                .setKd(0.3).setKs(0.8).setShininess(80).setkT(new Double3(0.5))),
+                new Polygon( // left wall
+                        new Point(-60, 60, 100),
+                        new Point(-100, 100, 200),
+                        new Point(-100, -100, 200),
+                        new Point(-60, -60, 100))
+                        .setEmission(new Color(170,250,255))
+                        .setMaterial(new Material()
+                                .setKd(0.3).setKs(0.8).setShininess(80).setkT(new Double3(0.5))),
+                new Polygon( // right wall
+                        new Point(100, 100, 200),
+                        new Point(60, 60, 100),
+                        new Point(60, -60, 100),
+                        new Point(100, -100, 200))
+                        .setEmission(new Color(170,250,255))
+                        .setMaterial(new Material()
+                                .setKd(0.3).setKs(0.8).setShininess(80).setkT(new Double3(0.5))),
+                new Polygon( // ceiling
+                        new Point(-60, 60, 100),
+                        new Point(60, 60, 100),
+                        new Point(100, 100, 200),
+                        new Point(-100, 100, 200))
+                        .setEmission(new Color(255, 255, 255))
+                        .setMaterial(new Material()
+                                .setKd(0.3).setKs(0.8).setShininess(80).setkT(new Double3(0.5))),
+
+                // CUPBOARD
+
+                new Polygon( // left
+                new Point(-20, 10, 100),
+                new Point(-30, 20, 110),
+                new Point(-30, -20, 110),
+                new Point(-20, -10, 100))
+                        .setEmission(new Color(205,150,55))
+                        .setMaterial(new Material()
+                                .setKd(0.3).setKs(0.7)),
+                new Polygon( // right
+                        new Point(20, 10, 100),
+                        new Point(30, 20, 110),
+                        new Point(30, -20, 110),
+                        new Point(20, -10, 100))
+                        .setEmission(new Color(205,150,55))
+                        .setMaterial(new Material()
+                                .setKd(0.3).setKs(0.7)),
+                new Polygon( // front
+                        new Point(-30, 20, 110),
+                        new Point(30, 20, 110),
+                        new Point(30, -20, 110),
+                        new Point(-30, -20, 110))
+                        .setEmission(new Color(205,150,55))
+                        .setMaterial(new Material()
+                                .setKd(0.3).setKs(0.7)),
+                new Polygon( // top
+                        new Point(-20, 10, 100),
+                        new Point(20, 10, 100),
+                        new Point(30, 20, 110),
+                        new Point(-30, 20, 110))
+                        .setEmission(new Color(255, 255, 255))
+                        .setMaterial(new Material()
+                                .setKd(0.3).setKs(0.8).setShininess(80).setkT(new Double3(0.5))),
+                new Polygon( // floor
+                        new Point(-30, -20, 110),
+                        new Point(30, -20, 110),
+                        new Point(20, -10, 100),
+                        new Point(-20, -10, 100))
+                        .setEmission(new Color(230, 190, 105)),
+
+
+                // DESK
+
+                new Polygon( // top
+                        new Point(-40, -40, 120),
+                        new Point(40, -40, 120),
+                        new Point(30, -30, 100),
+                        new Point(-30, -30, 100))
+                        .setEmission(new Color(230, 190, 105)),
+                new Polygon( // bottom
+                        new Point(-40, -45, 120),
+                        new Point(40, -45, 120),
+                        new Point(30, -35, 100),
+                        new Point(-30, -35, 100))
+                        .setEmission(new Color(230, 190, 105)),
+                new Polygon( // left
+                        new Point(-30, -60, 100),
+                        new Point(-30, -30, 100),
+                        new Point(-40, -40, 120),
+                        new Point(-40, -70, 120))
+                        .setEmission(new Color(230, 190, 105)),
+                new Polygon( // right
+                        new Point(30, -60, 100),
+                        new Point(30, -30, 100),
+                        new Point(40, -40, 120),
+                        new Point(40, -70, 120))
+                        .setEmission(new Color(230, 190, 105))
+
+//                new Polygon(
+//                        new Point(40, -45, 120),
+//                        new Point(36, -45, 120),
+//                        new Point(36,-70, 120),
+//                        new Point(40,-70, 120))
+//                        .setEmission(new Color(230, 190, 105)),
+//                new Polygon(
+//                        new Point(-40, -45, 120),
+//                        new Point(-36, -45, 120),
+//                        new Point(-36,-70,120),
+//                        new Point(-40,-70,120))
+//                        .setEmission(new Color(230, 190, 105))
+
+
+
+                );
+
+//        scene.lights.add(
+//                new SpotLight(new Color(400, 400, 400), new Point(-10, -10, 100), new Vector(0, -2, -2)).setKl(0.01).setKq(0.001),
+//                new SpotLight(new Color(255,255,255), new Point(0, 80, 150), new Vector(0,-1,0)).setKl(0.05).setKq(0.01));
+//        scene.lights.add(new SpotLight(new Color(400,400,400), new Point(30,-40,170),new Vector(0,0,-1)).setKl(0.01).setKq(0.001));
+
+        ImageWriter imageWriter = new ImageWriter("minip2", 600, 600);
+        camera.setImageWriter(imageWriter) //
+                .setRayTracerBase(new RayTracerBasic(scene)) //
+                .renderImage() //
+                .writeToImage();
+    }
+
     @Test
     public void finalPic3() {
         Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0))
@@ -109,94 +258,8 @@ scene.geometries.add(
                      .setEmission(new Color(200,105,80))
 //                     .setMaterial(new Material()
 //                             .setKd(0.5).setKs(0.5).setShininess(10))
-
-//
-//                new Triangle(a,g,h)
-//                        .setEmission(new Color(0, 75, 66))
-//                        .setMaterial(new Material()
-//                                .setkD(new Double3(0.6)).setkS(new Double3(0.4)).setkT(new Double3(0.6))
-//                                .setShininess(80)),
-//                new Triangle(a,b,h)
-//                        .setEmission(new Color(0, 75, 66))
-//                        .setMaterial(new Material()
-//                                .setkD(new Double3(0.6)).setkS(new Double3(0.4)).setkT(new Double3(0.6))
-//                                .setShininess(80)),
-//                new Triangle(a,b,g)
-//                        .setEmission(new Color(0, 75, 66))
-//                        .setMaterial(new Material()
-//                                .setkD(new Double3(0.6)).setkS(new Double3(0.4)).setkT(new Double3(0.6))
-//                                .setShininess(80)),
-//                new Triangle(g,b,h)
-//                        .setEmission(new Color(0, 75, 66))
-//                        .setMaterial(new Material()
-//                                .setkD(new Double3(0.6)).setkS(new Double3(0.4)).setkT(new Double3(0.6))
-//                                .setShininess(80))
         );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//                                          new Polygon(new Point(-25,-50,-30),
-//                                                         new Point(-25,-50,30),
-//                                                         new Point(15,-50,30),
-//                                                         new Point(15,-50,-30))
-//                                                         .setEmission(new Color(0,75,100))
-//                                                         .setMaterial(new Material()
-//                                                                 .setkD(new Double3(0.6)).setkS(new Double3(0.4))
-//                                                                 .setShininess(50)),
-//                                                 new Polygon(new Point(-25,-25,-30),
-//                                                         new Point(-25,-25,30),
-//                                                         new Point(15,-25,30),
-//                                                         new Point(15,-25,-30))
-//                                                         .setEmission(new Color(0,75,100))
-//                                                         .setMaterial(new Material()
-//                                                                 .setkD(new Double3(0.6)).setkS(new Double3(0.4))
-//                                                                 .setShininess(50))
-//
-//
-//
-
-
-
-//
-//                        new Polygon(
-//                                new Point(130, -60, -60), //
-//                                new Point(-100, -60, 60),
-//                                new Point(-100, -100, 60),  //
-//                                new Point(130, -100, -60))  //
-////                new Polygon(
-////
-////                        new Point(-120, -120, 500),
-////                        new Point(-80, -120, 500),
-////                        new Point(120, 60, 100),
-////                        new Point(160, 60, 100))
-//                        .setEmission(new Color(105,105,105))
-//                        .setMaterial(new Material())
-
-//
-//        scene.lights.add(
-//                new SpotLight(
-//                        new Color(400, 400, 400),
-//                        new Vector(-0.5, -1, -0.5),
-//                        new Vector(-1, 0, 1))
-//                        .setKl(0.004)
-//                        .setKq(0.000006));
-//        scene.lights.add(new SpotLight(new Color(255,228,181), new Point(80, 50, 0), new Vector(0, -1, 0)) //
-//                .setKl(4E-5).setKq(2E-7));
         scene.lights.add(new SpotLight(new Color(800, 500, 250), new Point(30, 10, -100), new Vector(-2, -2, -2)).setKl(0.001).setKq(0.0001));
         ImageWriter imageWriter = new ImageWriter("finalPic", 600, 600);
         camera.setImageWriter(imageWriter) //
@@ -218,100 +281,8 @@ scene.geometries.add(
 
 
 
-    @Test
-    public void finalPic() {
-        Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0))
-                .setVPSize(200, 200).setVPDistance(1000);
-        scene.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
-
-        scene.geometries.add(
-                new Polygon(
-                        new Point(-95, -80, -60),
-                        new Point(-100, -100, 60),
-                        new Point(100, -100, 60),
-                        new Point(95, -80, -60))
-                        .setEmission(new Color(0, 150, 200))
-                        .setMaterial(new Material()
-                                .setkD(new Double3(0.6)).setkS(new Double3(0.4))
-                                .setShininess(50)),
-                new Polygon(
-                        new Point(-95, -80, -60),
-                        new Point(-70, 60, 200),
-                        new Point(70, 60, 200),
-                        new Point(95, -80, -60))
-                        .setEmission(new Color(0, 255, 255))
-                        .setMaterial(new Material()
-                                .setKd(0.3).setKs(0.8).setShininess(80))
-                , new Polygon(
-                        new Point(-95, 120, -60),
-                        new Point(-70, 60, 200),
-                        new Point(70, 60, 200),
-                        new Point(95, 120, -60))
-                        .setEmission(new Color(255, 255, 0))
-                        .setMaterial(new Material()
-                                .setKd(0.3).setKs(0.8).setShininess(80))
 
 
-                , new Sphere(new Point(-95, -80, -60), 5)
-                        .setEmission(new Color(0, 255, 0))
-                        .setMaterial(new Material()
-                                .setKd(0.3).setKs(0.8).setShininess(80))
-                , new Sphere(new Point(-70, 60, 200), 5)
-                        .setEmission(new Color(255, 0, 255))
-                        .setMaterial(new Material()
-                                .setKd(0.3).setKs(0.8).setShininess(80))
-                , new Sphere(new Point(70, 60, 200), 5)
-                        .setEmission(new Color(0, 0, 255))
-                        .setMaterial(new Material()
-                                .setKd(0.3).setKs(0.8).setShininess(80))
-                , new Sphere(new Point(95, -80, -60), 5)
-                        .setEmission(new Color(255, 0, 0))
-                        .setMaterial(new Material()
-                                .setKd(0.3).setKs(0.8).setShininess(80))
-                , new Sphere(new Point(0, 0, 0), 5)
-                        .setEmission(new Color(255, 0, 0))
-                        .setMaterial(new Material()
-                                .setKd(0.3).setKs(0.8).setShininess(80))
-
-
-        );
-
-        scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(60, 50, 0), new Vector(0, 0, -1)) //
-                .setKl(4E-5).setKq(2E-7));
-
-        ImageWriter imageWriter = new ImageWriter("targil7pic", 600, 600);
-        camera.setImageWriter(imageWriter) //
-                .setRayTracerBase(new RayTracerBasic(scene)) //
-                .renderImage() //
-                .writeToImage();
-    }
-
-    @Test
-    public void finalPic2() {
-        Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0))
-                .setVPSize(200, 200).setVPDistance(1000);
-        scene.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
-
-        scene.geometries.add(
-//                new Polygon(
-//                        new Point(-120, -120, 500),
-//                        new Point(-80, -160, 550),
-//                        new Point(120, 60, 200),
-//                        new Point(160, 20, 250))
-////                        new Point(-95, -50, -60),
-////                        new Point(-100, -100, 60),
-////                        new Point(100, -100, 60),
-////                        new Point(45, -50, -60))
-//                        .setEmission(new Color(115,115,115))
-//                        .setMaterial(new Material()),
-        );
-
-        ImageWriter imageWriter = new ImageWriter("finalPic", 600, 600);
-        camera.setImageWriter(imageWriter) //
-                .setRayTracerBase(new RayTracerBasic(scene)) //
-                .renderImage() //
-                .writeToImage();
-    }
 
     @Test
     public void tryStuff() {
